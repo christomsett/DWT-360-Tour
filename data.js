ce1ae88fe5f0ce3632d1ec39228aa702fa21333a
Chris Tomsett <c.g.tomsett@soton.ac.uk>
2025-07-24 12:55:13 +0100

    Initial Commit
    Upload of 360 Tour

---
+++ data.js
@@ -0,0 +1,529 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sssi",
+      "name": "SSSI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1343486006334125,
+        "pitch": -0.16260463281102844,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0787548515643515,
+          "pitch": -0.07216179180843874,
+          "rotation": 0,
+          "target": "8-gate"
+        },
+        {
+          "yaw": 0.8871480677663879,
+          "pitch": 0.1087167156026716,
+          "rotation": 0,
+          "target": "1-bowl-freshly-cut"
+        },
+        {
+          "yaw": 0.42089271752679736,
+          "pitch": -0.029608581806384038,
+          "rotation": 0,
+          "target": "7-grazing"
+        },
+        {
+          "yaw": 1.4831369971736796,
+          "pitch": 0.030298873076610278,
+          "rotation": 0,
+          "target": "3-chapel-and-yard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5970618690026637,
+          "pitch": 0.08798537344620883,
+          "title": "Chalk downland habitat",
+          "text": "<p class=\"MsoNormal\">This raised 'horseshoe' area of Lyscombe is a Site of Special Scientific\nInterest – more information about what this means, and how this\nhabitat is being protected in the video above.&nbsp;<o:p></o:p></p>"
+        },
+        {
+          "yaw": -0.12003949094304289,
+          "pitch": -0.004580162593349257,
+          "title": "Butterflies",
+          "text": "<div><img src=\"https://res-console.cloudinary.com/dapxot2mu/thumbnails/v1/image/upload/v1753354424/OV9HcmVlbl9oYWlyc3RyZWFrX0Jlbl9BdGtpbnNvbl9fX2hhbGZfY3JvcHBlZF9DdXN0b21fY2Mxc2cw/drilldown\" alt=\"9_Green_hairstreak_Ben_Atkinson___half_cropped_Custom_cc1sg0\">Green hairstreak butterflies can be spotted on the downland slopes of this SSSI, where their caterpillar foodplant grows<font face=\"Aptos, sans-serif\"><span style=\"font-size: 16px;\">.</span></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-bowl-freshly-cut",
+      "name": "Bowl (freshly cut)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.5866071516167208,
+        "pitch": 0.013354286787569336,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.170660449260149,
+          "pitch": 0.016262188784967435,
+          "rotation": 0,
+          "target": "2-bowl-flowering"
+        },
+        {
+          "yaw": -1.8006421749482016,
+          "pitch": -0.24506575093307603,
+          "rotation": 0,
+          "target": "0-sssi"
+        },
+        {
+          "yaw": 0.6132836474524872,
+          "pitch": -0.14731550527066162,
+          "rotation": 0,
+          "target": "7-grazing"
+        },
+        {
+          "yaw": -2.4212035968012806,
+          "pitch": -0.239157478964783,
+          "rotation": 0,
+          "target": "8-gate"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4756971522074531,
+          "pitch": 0.14242492009440788,
+          "title": "The Bowl",
+          "text": "<p class=\"MsoNormal\">The fields within Lyscombe bottom, sometimes referred to as\nthe bowl, were restored to species rich grasslands around 20 years ago.</p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">They\nare now full of wildflowers including several orchid species.&nbsp;</span><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">A mixture of hay\ncutting and grazing will maintain their species richness.&nbsp;</span></p>"
+        },
+        {
+          "yaw": -0.25830442057939784,
+          "pitch": -0.0006934406985870112,
+          "title": "Common orchids spotted in the Bowl",
+          "text": "<img src=\"https://res-console.cloudinary.com/dapxot2mu/thumbnails/v1/image/upload/v1753345923/M19Db21tb25fc3BvdHRlZF9vcmNoaWRzX2luX3RoZV9ib3dsX0Jlbl9BdGtpbnNvbl9jcm9wcGVkX0N1c3RvbV8yX2pqcXpwbA==/drilldown\" alt=\"3_Common_spotted_orchids_in_the_bowl_Ben_Atkinson_cropped_Custom_2_jjqzpl\">"
+        }
+      ]
+    },
+    {
+      "id": "2-bowl-flowering",
+      "name": "Bowl (flowering)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.026808234655110397,
+        "pitch": -0.02715561195115157,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4246413240605618,
+          "pitch": -0.14820533920754997,
+          "rotation": 0,
+          "target": "7-grazing"
+        },
+        {
+          "yaw": 1.2022280222249542,
+          "pitch": -0.1563057240145138,
+          "rotation": 0,
+          "target": "0-sssi"
+        },
+        {
+          "yaw": 1.6659276224262802,
+          "pitch": -0.05383934993460571,
+          "rotation": 0,
+          "target": "1-bowl-freshly-cut"
+        },
+        {
+          "yaw": -2.7938815053717185,
+          "pitch": -0.010711332187415934,
+          "rotation": 0,
+          "target": "3-chapel-and-yard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9752229346062098,
+          "pitch": -0.30369968336548325,
+          "title": "Duke of burgundy",
+          "text": "<p class=\"MsoNormal\"><img src=\"https://res-console.cloudinary.com/dapxot2mu/thumbnails/v1/image/upload/v1753352824/RHVrZV9vZl9CdXJndW5keV9KYW1lc19DYXJ0d3JpZ2h0X19feWVsbG93X2Nyb3BwZWRfQ3VzdG9tX2JvdWp2eQ==/drilldown\" alt=\"Duke_of_Burgundy_James_Cartwright___yellow_cropped_Custom_boujvy\" style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\"></p><p class=\"MsoNormal\"><span style=\"font-family: inherit; font-size: inherit; font-style: inherit; font-variant-ligatures: inherit; font-variant-caps: inherit; font-weight: inherit;\">Over this hill is a known colony of the endangered Duke\nof burgandy butterfly, one of the rarest butterflies in the UK, found\nin only a handful of sites in Dorset.&nbsp;</span></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\">Work is underway to encourage these\nbutterflies to spread onto Lyscombe to help secure their future.</p>"
+        },
+        {
+          "yaw": -0.08507541509397143,
+          "pitch": -0.1748219832540805,
+          "title": "Marsh fritillary",
+          "text": "<div><p class=\"MsoNormal\"><span lang=\"EN-US\">One of the\nrarest butterflies in the UK, the marsh fritillary, can be found on the downland here.&nbsp;</span></p><p class=\"MsoNormal\"><span lang=\"EN-US\"><img src=\"https://res-console.cloudinary.com/dapxot2mu/thumbnails/v1/image/upload/v1753345764/NF9KYW1lc19DYXJ0d3JpZ2h0X01hcnNoX0ZyaXRpbGxhcnlfX19jcm9wZWRfcmVzaXplZF9ic3B2a3M=/drilldown\" alt=\"4_James_Cartwright_Marsh_Fritillary___croped_resized_bspvks\"></span></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-chapel-and-yard",
+      "name": "Chapel and Yard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.9804781058664602,
+        "pitch": -0.18820649551760127,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6506993641384895,
+          "pitch": -0.09801743208958591,
+          "rotation": 0,
+          "target": "4-the-little-piddle"
+        },
+        {
+          "yaw": 2.320744200019746,
+          "pitch": 0.055601815786902264,
+          "rotation": 0,
+          "target": "5-chapel"
+        },
+        {
+          "yaw": 1.984146199253261,
+          "pitch": -0.3053316691122383,
+          "rotation": 0,
+          "target": "6-former-arable-land"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6511140894234977,
+          "pitch": -0.029029908752269407,
+          "title": "Yard",
+          "text": "<p class=\"MsoNormal\">This concrete yard and modern agricultural buildings are\nbeing removed to allow the Little Piddle to take its natural course and restore\nthe area to a wildflower meadow. <o:p></o:p></p><p class=\"MsoNormal\"><br></p>\n\n<p class=\"MsoNormal\">The older barns to the east of the yard are privately\nowned and are being restored.<i><o:p></o:p></i></p>"
+        },
+        {
+          "yaw": 1.2867870118403548,
+          "pitch": -0.16677533949078338,
+          "title": "Restoration",
+          "text": "<p class=\"MsoNormal\">Lyscombe’s medieval Chapel and Priest’s house had been in\nruin since the 1950’s, but were expertly restored in 2005 with support\nfrom English Heritage and DEFRA. <o:p></o:p></p><p class=\"MsoNormal\"><br></p>\n\n<p class=\"MsoNormal\">Dorset Wildlife Trust will be maintaining these historical\nbuildings which act as a focal point for the nature reserve and offer a\ntangible link to its past.<i><o:p></o:p></i></p>"
+        }
+      ]
+    },
+    {
+      "id": "4-the-little-piddle",
+      "name": "The Little Piddle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.22022691219191337,
+        "pitch": -0.25081150620480486,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9467950794565709,
+          "pitch": -0.022028211609562476,
+          "rotation": 0,
+          "target": "3-chapel-and-yard"
+        },
+        {
+          "yaw": 2.261277293128038,
+          "pitch": -0.15631789742826108,
+          "rotation": 0,
+          "target": "2-bowl-flowering"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.24179305544100238,
+          "pitch": -0.1876711954456809,
+          "title": "River restoration",
+          "text": "<p class=\"MsoNormal\">Downstream, impoundments will be removed and woody\ndebris dams will be created to help reconnect the river with its floodplain. Holding water on site for longer will create more valuable wetland habitat.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "5-chapel",
+      "name": "Chapel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.02849517146113456,
+        "pitch": 0.025023481862033847,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6525004003133903,
+          "pitch": 0.021532720110206682,
+          "rotation": 0,
+          "target": "3-chapel-and-yard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-former-arable-land",
+      "name": "Former arable land",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.3464482787822032,
+        "pitch": -0.1643832055142873,
+        "fov": 1.5600227277000773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.59820985733308,
+          "pitch": -0.1417953113966881,
+          "rotation": 0,
+          "target": "7-grazing"
+        },
+        {
+          "yaw": 1.6962378179978952,
+          "pitch": -0.07243002703683743,
+          "rotation": 0,
+          "target": "3-chapel-and-yard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.18115104786597414,
+          "pitch": 0.12633640879734997,
+          "title": "Encouraging wildflowers",
+          "text": "<p class=\"MsoNormal\">This large field, like many others, has been cut for hay to\nremove the nutrients in the grass and decrease the fertility of the soil. In time, this\nwill favour wildflowers, increasing the species diversity of the site.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "7-grazing",
+      "name": "Grazing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.7138154205705582,
+        "pitch": -0.05338875357755768,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.995439687557833,
+          "pitch": -0.03993492869235382,
+          "rotation": 0,
+          "target": "0-sssi"
+        },
+        {
+          "yaw": -1.5241054822512439,
+          "pitch": -0.022246172322939017,
+          "rotation": 0,
+          "target": "6-former-arable-land"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4358635455421087,
+          "pitch": 0.040745738384401875,
+          "title": "English Longhorn cattle",
+          "text": "<p class=\"MsoNormal\">This hardy breed is grazing the grasslands, preventing scrub\nand coarser grasses from dominating.</p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\">They help create a more complex habitat,\nspreading wildflowers on their fur and dung, debarking with their horns, and\ncreating bare ground with their hooves - ideal for wildflowers to germinate and\ninsects to nest and bask in!</p>"
+        },
+        {
+          "yaw": 0.701930961640647,
+          "pitch": 0.2340047954461575,
+          "title": "Grazing",
+          "text": "<p class=\"MsoNormal\">Initially the cattle are grazing the SSSI grasslands, in the\nfuture they will be allowed free roam over the rest of the site.<o:p></o:p></p>"
+        }
+      ]
+    },
+    {
+      "id": "8-gate",
+      "name": "Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.6386598171285769,
+        "pitch": 0.24426874632039564,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5183955523231898,
+          "pitch": 0.02633716925875973,
+          "rotation": 0,
+          "target": "0-sssi"
+        },
+        {
+          "yaw": 1.940101781872066,
+          "pitch": 0.1621541017911401,
+          "rotation": 0,
+          "target": "1-bowl-freshly-cut"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5254202710807583,
+          "pitch": 0.3887256907136365,
+          "title": "Footpaths",
+          "text": "<p class=\"MsoNormal\">Lyscombe has several bridleways and public footpaths\nwhich allow access through the reserve and into the wider countryside.<i><o:p></o:p></i></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Lyscombe Nature Reserve",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
